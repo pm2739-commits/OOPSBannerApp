340f85dd5cfcf7db969488fdb527e85a3f7d0827
Priytush <pm2739@srmist.edu.in>
2026-03-10 03:47:25 +0530

    UC3: Refactored banner using String.join()

---
+++ UC3_OOPS_Banner.java
@@ -0,0 +1,21 @@
+public class UC3_OOPS_Banner {
+    public static void main(String[] args) {
+        String line1 = String.join("", "O","O","O","O","O","O","O");
+        String line2 = String.join("", "O","     ","O");
+        String line3 = String.join("", "O","     ","O");
+        String line4 = String.join("", "O","O","O","O","O","O","O");
+        String line5 = String.join("", "     ","P","P","P");
+        String line6 = String.join("", "     ","P","   ","P");
+        String line7 = String.join("", "     ","P","P","P");
+        String line8 = String.join("", "     ","S","S","S","S","S");
+
+        System.out.println(line1);
+        System.out.println(line2);
+        System.out.println(line3);
+        System.out.println(line4);
+        System.out.println(line5);
+        System.out.println(line6);
+        System.out.println(line7);
+        System.out.println(line8);
+    }
+}
